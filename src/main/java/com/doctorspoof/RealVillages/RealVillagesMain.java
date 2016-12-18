df758d3e02421d177abb261f1c733303a76d20d1
doctorspoof <calumhaigh@hotmail.com>
2016-12-18 11:59:21 +0000

    Initial commit

---
+++ src/main/java/com/doctorspoof/RealVillages/RealVillagesMain.java
@@ -0,0 +1,11 @@
+package com.doctorspoof.RealVillages;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = RealVillagesMain.MODID, version = RealVillagesMain.VERSION)
+
+public class RealVillagesMain 
+{
+	public static final String MODID = "realvillages";
+    public static final String VERSION = "1.0";
+}
